1013ac578933d9ad2ce1f9a5838307140b369925
Thuc Katy <katyperrycbt@gmail.com>
2020-11-19 13:07:18 +0700

    Done Credential

--- sms.js
+++ sms.js
@@ -52,10 +52,6 @@ app.use('/school', authMiddleware.default, authMiddleware.requireAuth, csrfProte
 
 app.listen(port, function () {
   console.log('Server at port '  + port + ' is running...!');
-  console.log('Ji');
-  console.log('Ji');
-  console.log('Ji');
-  console.log('Ji');
-  console.log('Ji');
+
   open('http://localhost:6969', {app: edge});
 });
